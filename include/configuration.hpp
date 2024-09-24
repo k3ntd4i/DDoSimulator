efbc8eab58dd01f8203e9fc64b5f40c038242816
k3ntd4i <ackar_quentdie48@outlook.com>
2024-09-23 21:01:24 -0500

    Configuracion del fondo con el mapa

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -4,3 +4,4 @@
 #include "SFML/Window.hpp"
 
 void initialize_window(sf::RenderWindow &window, sf::View &view);
+void initialize_map(sf::Texture &texture_background, sf::Sprite &sprite_background);
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include "../include/configuration.hpp"
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
+#include <stdexcept>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window{};
     sf::View view{};
 
+    sf::Texture texture_background{};
+    sf::Sprite sprite_background{};
+
     initialize_window(window, view);
+    try
+    {
+        initialize_map(texture_background, sprite_background);
+    }
+    catch (const std::runtime_error &error)
+    {
+        std::cerr << "Error in initialization. " << error.what();
+        return 1;
+    }
 
     while (window.isOpen())
     {
@@ -26,6 +40,7 @@ int main()
         }
 
         window.clear(sf::Color::Black);
+        window.draw(sprite_background);
         window.display();
     }
 
