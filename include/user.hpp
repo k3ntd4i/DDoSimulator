a5dfcfa3ba8b67ece47e531b953753b8711e40d4
k3ntd4i <ackar_quentdie48@outlook.com>
2024-09-25 23:08:47 -0500

    Correccion nombre criptomoneda

--- include/user.hpp
+++ include/user.hpp
@@ -3,7 +3,7 @@
 class User
 {
     int anonymity{ 5 };
-    int yukacoins{ 0 };
+    int yuca_coins{ 0 };
     int hack_intensity{ 10 };
     bool captured{ false };
 
@@ -11,11 +11,11 @@ public:
     User() = default;
 
     int get_anonymity() { return this->anonymity; }
-    int get_yukacoins() { return this->yukacoins; }
+    int get_yuca_coins() { return this->yuca_coins; }
     int get_hack_intensity() { return this->hack_intensity; }
     bool was_captured() { return this->captured; }
     void increase_hack_intensity(int new_intesity) { this->hack_intensity += new_intesity; }
-    void update_yukacoins_wallet(int cash_flow) { this->yukacoins += cash_flow; }
+    void update_yuca_coins_wallet(int cash_flow) { this->yuca_coins += cash_flow; }
 
     void decrease_anonymity()
     {
